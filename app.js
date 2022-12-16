e8894a8aa11cf4cd25729660703b425d50c68e00
syndelldev <priyankap.syndell@gmail.com>
2022-12-16 14:13:58 +0530

    Update app.js
    create csv data export

--- app.js
+++ app.js
@@ -2,38 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 var mysql = require('mysql')
+const reader = require('xlsx')
 
-
-
+// First Page APIs
 app.get('/viewersApi', (req, res) => {
   res.end('55.22M');
-
-      // fs.readFile(`graphapi/userapi.json`, "utf-8", (err, data) => {
-      //   res.end(data);    
-      // });
-
-    // var con = mysql.createConnection({
-    //     host: "127.0.0.1",
-    //     user: "root",
-    //     password: "",
-    //     database: "dict"
-    //   });
-
-    //   con.connect(function(err) {
-    //     if (err) throw err;
-    //     //Select all customers and return the result object:
-    //     con.query("SELECT * FROM chart_data", function (err, result, fields) {
-    //         if (err) throw err;
-    //         var scoreHome = [];
-    //         result.forEach(element => {
-    //             console.log(element.score);
-    //             scoreHome.push(element.score)
-    //         });
-    //         var l = JSON.stringify(scoreHome)  
-    //         console.log(l);
-    //         res.end(l);
-    //     });
-    //   });  
 });
 
 app.get('/watchTimeApi', (req, res) => {
@@ -42,6 +15,96 @@ app.get('/watchTimeApi', (req, res) => {
 
 app.get('/adApi', (req, res) => {
   res.end('355.55 M');
+});
+
+// Second Page APIs
+app.get('/linearReachApi', (req, res) => {
+  res.end('110.36M');
+});
+
+app.get('/linearWatchTimeApi', (req, res) => {
+  res.end('543.23 Min');
+});
+
+
+app.get('/ottViewersApi', (req, res) => {
+  res.end('65.22 Min');
+});
+
+app.get('/ottWatchTimeApi', (req, res) => {
+  res.end('143.45 Min');
+});
+
+app.get('/adTimeApi', (req, res) => {
+  res.end('3245.12 Min');
+});
+
+
+app.get('/csvApi', (req, res) => {
+  // Reading our test file
+  const file = reader.readFile('test.csv')
+
+    
+  let data = []
+    
+  const sheets = file.SheetNames
+    
+  for(let i = 0; i < sheets.length; i++)
+  {
+    const temp = reader.utils.sheet_to_json(
+          file.Sheets[file.SheetNames[i]])
+    temp.forEach((res) => {
+        data.push(res)
+    })
+  }
+  
+  
+// Printing data
+console.log(data)
+
+      var scoreHome = 0;
+      result.forEach(element => {
+          // console.log(element.score);
+          scoreHome = scoreHome + element.score;
+      });
+      var l = JSON.stringify(scoreHome)  
+      console.log('sum is:', scoreHome)
+      res.end(JSON.stringify(scoreHome));
+
+
+
+
+  // console.log('file is:', file)
+
+  // res.end(JSON.stringify(file));
+});
+
+
+
+
+app.get('/sumAPIs', (req, res) => {
+  var con = mysql.createConnection({
+       host: "127.0.0.1",
+       user: "root",
+       password: "",
+       database: "dict"
+     });
+
+     con.connect(function(err) {
+       if (err) throw err;
+       //Select all customers and return the result object:
+       con.query("SELECT * FROM chart_data", function (err, result, fields) {
+           if (err) throw err;
+           var scoreHome = 0;
+           result.forEach(element => {
+               // console.log(element.score);
+               scoreHome = scoreHome + element.score;
+           });
+           var l = JSON.stringify(scoreHome)  
+           console.log('sum is:', scoreHome)
+           res.end(JSON.stringify(scoreHome));
+       });
+     });  
 });
 
 app.listen(port, () => {
