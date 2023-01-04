a6ed366e96c797cf48912496d2766e62d6b29a50
milonishah9 <107255075+milonishah9@users.noreply.github.com>
2023-01-04 13:16:19 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 var mysql = require('mysql')
 
-
+fetch('https://zeel-my.sharepoint.com/:x:/r/personal/vc_miloni_shah_zee_com/_layouts/15/Doc.aspx?sourcedoc=%7B832418BC-00D1-4FF8-AE85-CCBAF1F968F7%7D&file=Landing%20Page%20_Executive%20View_updated%20(1).xlsx&action=default&mobileredirect=true&cid=aad12f55-e959-4742-b7ec-9ab497a99878').then(res => JSON.parse(res)).then(result => console.log(result))
 
 app.get('/viewersApi', (req, res) => {
   res.end('55.22M');
