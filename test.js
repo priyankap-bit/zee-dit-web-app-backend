bfe451f96d3fc5a4e3ac436e48a65de7cd93c1e2
priyankap-bit <106512706+priyankap-bit@users.noreply.github.com>
2023-01-04 14:22:51 +0530

    Created file

---
+++ test.js
@@ -0,0 +1,1 @@
+const https = require('https'); https.get('https://example.com/file.csv', (response) => { let data = ''; // A chunk of data has been received. response.on('data', (chunk) => { data += chunk; }); // The whole response has been received. Print out the result. response.on('end', () => { console.log(data); }); }).on("error", (err) => { console.log("Error: " + err.message); });
